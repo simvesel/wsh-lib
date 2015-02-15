0bf64cbfa79e776dc3003875748fd14a28f1cf31
slava <simvesel@users.noreply.github.com>
2015-02-16 01:57:01 +0300

    Актуализировал код по проверки правильности регистрации компонента в ОС, что он доступен и работает

--- src/verify_component_registration.js
+++ src/verify_component_registration.js
@@ -4,25 +4,12 @@
 //var myComp = new ActiveXObject("{5183B7A6-9D7D-4ED7-8CF7-48E832C8DAD1}");
 //var myComp = new ActiveXObject("5183B7A6-9D7D-4ED7-8CF7-48E832C8DAD1");
 
-var myComp = new ActiveXObject("SLV.JS.Import");
+var g_cJSImport = new ActiveXObject("SLV.JS.Import");
 
-eval( myComp.getSourceCode() );
-//g_ws.Echo( myComp.getSourceCode() );
-//var masType = typeof myComp.getSourceCode();
-//g_ws.Echo( masType );
+eval( g_cJSImport.fn_import( "utils exec" ) );
+echo( g_cJSImport.xasOwnPath, g_cJSImport.xasLibDirPath );
 
-g_ws.Echo( myComp.xasOwnPath, myComp.xasLibDirPath );
-
-/*
-import alias;
-import sysutils;
-import extended_string;
-
-import=alias;
-import=sysutils;
-*/
-
-eval( g_inc( "func_alias.js" ) );
-echo( 'pi-i-ip!', 'egerg', 81522709 );
-
-eval( g_inc( "sys_utils.js" ) );
+echo( 'Windows-1251: Привет, Мир!',
+		'UTF-8: РџСЂРёРІРµС‚, РњРёСЂ!',
+		'Test... Test... To-o-o-o-o-o-o-o Lo-o-o-o-o-ong (TL;DR)!',
+		81522709 );
