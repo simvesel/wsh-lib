bd9a83ca87183a18e293dd7e4bae0a1f655650b0
slava <simvesel@users.noreply.github.com>
2015-03-01 16:17:55 +0300

    В компоненте сделал проверку случая, когда вызывается функция импорта без параметров ("импорт по умолчанию"). Подправил код проверки регистрации компонента в системе.

--- src/verify_component_registration.js
+++ src/verify_component_registration.js
@@ -11,12 +11,20 @@
 //var myComp = new ActiveXObject("{5183B7A6-9D7D-4ED7-8CF7-48E832C8DAD1}");
 //var myComp = new ActiveXObject("5183B7A6-9D7D-4ED7-8CF7-48E832C8DAD1");
 
-var g_cJSImport = new ActiveXObject("SLV.JS.Import");
+var g_cJSImport = new ActiveXObject( "SLV.JS.Import" );
 
-eval( g_cJSImport.fn_import( "utils exec" ) );
-echo( g_cJSImport.xasOwnPath, g_cJSImport.xasLibDirPath );
+var masImp;
+//masImp = g_cJSImport.fn_import( "utils exec" );
+//masImp = g_cJSImport.fn_import( "" );
+masImp = g_cJSImport.fn_import();
 
-echo( 'Windows-1251: Привет, Мир!',
+eval( masImp );
+//echo( masImp );
+masImp = null;
+
+dbg_echo( 'Windows-1251: Привет, Мир!',
 		'UTF-8: РџСЂРёРІРµС‚, РњРёСЂ!',
 		'Test... Test... To-o-o-o-o-o-o-o Lo-o-o-o-o-ong (TL;DR)!',
 		81522709 );
+
+echo( g_cJSImport.xasOwnPath, "\n", g_cJSImport.xasLibDirPath );
